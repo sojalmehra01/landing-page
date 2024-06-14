dc8439eac093af63462f3affe8948624294b238f
Sojal Mehra <sojalmehra0619@gmail.com>
2024-06-14 18:11:27 +0530

    sorted

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="fixed z-[999] w-full px-20 py-8 font-['Neue Montreal'] flex justify-between item-center">
+    <div className="fixed z-[999] w-full px-20 py-8 font-['Neue Montreal'] flex justify-between items-center backdrop-blur-[3px] h-10">
           <div className="logo text-2xl font-bold ">
               kann-ban
           </div>
---
+++ src/Components/Navcomponents/Contact.jsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_3c2hqou', 'template_cqg5g7f', form.current, {
+        publicKey: 'J9vhCWeV9wFQoBWFm',
+      })
+      .then(
+        () => {
+          alert('Message sent Successfully!');
+        },
+        (error) => {
+          alert('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="from_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
